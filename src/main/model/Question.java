b23ee620a0db44786300e13b5487d98ed7114752
Fazal <zkansari@students.cs.ubc.ca>
2026-02-15 16:11:40 -0800

    Added a Question class

---
+++ src/main/model/Question.java
@@ -0,0 +1,22 @@
+package model;
+
+// Represents a question having a text String value questionText and a String category (one of intuition, sensing, feeling, judging)
+public class Question {
+    private String text;
+    private String category;
+    
+    // REQUIRES: text != null && text.length() > 0, and category != null 
+    // EFFECTS: text of question is set to questionText
+    public Question(String text, String category) {
+    }
+
+// EFFECTS: returns text, null if empty
+    public void getText() {
+
+    }
+
+// EFFECTS: returns category
+    public void getCategory() {
+
+    }
+}
