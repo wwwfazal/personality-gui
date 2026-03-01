da979bf08e8bcf9c081096163685641b8b29390f
Fazal <zkansari@students.cs.ubc.ca>
2026-03-01 15:11:16 -0800

    Added json test jar to lib, and empty classes for JsonReader, JsonWriter, Writable

---
+++ src/main/persistence/JsonWriter.java
@@ -0,0 +1,11 @@
+package persistence;
+import model.WorkRoom;
+import org.json.JSONObject;
+
+
+import java.io.*;
+
+
+// Represents a writer that writes JSON representation of personality database to file
+public class JsonWriter {
+}
