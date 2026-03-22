ccde1f8d1f24371e02bc5812f3b17e07c9dbf371
Fazal <zkansari@students.cs.ubc.ca>
2026-03-22 14:48:59 -0700

    Fixed bug where sensing percentage was shown as thinking percentage and personality test was not showing a gray wheel when empty

--- src/main/ui/PiChart.java
+++ src/main/ui/PiChart.java
@@ -17,24 +17,28 @@ public class PiChart extends JPanel {
     public PiChart(PersonalityTest personalityTest) {
         this.personalityTest = personalityTest;
         setPreferredSize(new Dimension(400, 300));
+        setBackground(Color.WHITE);
     }
 
     public void setTest(PersonalityTest personalityTest) {
         this.personalityTest = personalityTest;
         repaint();
     }
+    
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        if (personalityTest == null) {
+        if (personalityTest == null  || personalityTest.getTotalAnswers() == 0) {
+            g.setColor(Color.LIGHT_GRAY);
+            g.fillOval(100, 50, 200, 200);
             return;
         }
 
         int intuition = personalityTest.getIntuitionPercentage();
         int thinking = personalityTest.getThinkingPercentage();
-        int sensing = personalityTest.getThinkingPercentage();
+        int sensing = personalityTest.getSensingPercentage();
         int feeling = personalityTest.getFeelingPercentage();
 
         int[] values = {intuition, thinking, sensing, feeling};
@@ -49,6 +53,15 @@ public class PiChart extends JPanel {
             g.fillArc(100, 50, 200, 200, startPi, angle);
             startPi += angle;
         }
+
+        g.setColor(Color.GREEN);
+        g.drawString("Intuition", 320, 80);
+        g.setColor(Color.BLUE);
+        g.drawString("Thinking", 320, 100);
+        g.setColor(Color.ORANGE);
+        g.drawString("Sensing", 320, 120);
+        g.setColor(Color.PINK);
+        g.drawString("Feeling", 320, 140);
 
     }
 
